b143e60d78c46dfc1df68a9eacb337af44b0dfa5
Paolo Patrone <paolo.patrone99@gmail.com>
2023-05-17 12:37:04 +0200

    Multiple apps in a single page

--- multiple.js
+++ multiple.js
@@ -1,19 +1,5 @@
-import { loadMicroApp } from '../../es';
+import 'zone.js';   // for angular subapp
+import { loadMicroApp } from 'qiankun';
 
-let app;
-
-function mount() {
-  app = loadMicroApp(
-    { name: 'react15', entry: '//localhost:7102', container: '#react15' },
-    { sandbox: { experimentalStyleIsolation: true } },
-  );
-}
-
-function unmount() {
-  app.unmount();
-}
-
-document.querySelector('#mount').addEventListener('click', mount);
-document.querySelector('#unmount').addEventListener('click', unmount);
-
-loadMicroApp({ name: 'vue', entry: '//localhost:7101', container: '#vue' });
+loadMicroApp({ name: 'react', entry: '//localhost:3000', container: '#react-app' });
+loadMicroApp({ name: 'angular', entry: '//localhost:4200', container: '#angular-app' });
